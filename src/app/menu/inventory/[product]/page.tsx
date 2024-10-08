0b379ff44d610457d1b0392c7428f999853e9e86
Aquarat <aquarat.t@ku.th>
2024-10-08 09:52:37 +0700

    create_inventory_page

---
+++ src/app/menu/inventory/[product]/page.tsx
@@ -0,0 +1,7 @@
+export default function ProductInInventoryPage() {
+    return (
+        <>
+            <h2>Helllllllllll</h2>
+        </>
+    )
+}
---
+++ src/app/menu/inventory/page.tsx
@@ -0,0 +1,9 @@
+import "@/globals.css"
+
+export default function InventoryPage() {
+    return (
+        <>
+            <h1 className="bg-grey">Hello world</h1>
+        </>
+    )
+}
